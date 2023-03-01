297053627167b36331de9d0176339d9b6368309d
Juan Manuel Androetto <juan.androetto36@gmail.com>
2023-03-01 12:53:05 -0300

    correccion title main section

--- src/components/MainSection/MainSection.styles.ts
+++ src/components/MainSection/MainSection.styles.ts
@@ -65,6 +65,7 @@ export const StyledContainerDescription = styled("div", {
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "flex-start",
+    width: "40%",
   },
 });
 
